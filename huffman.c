4962be975bead9f9f454cc2d13831f9e9bcca250
Milena Nunes <38266476+lenna2002@users.noreply.github.com>
2019-03-11 15:06:48 -0300

    insercao na arvore com o arquivo
    arvore correta com os caracteres do arquivo .txt

--- huffman.c
+++ huffman.c
@@ -26,18 +26,6 @@ priority_queue* create_priority_queue ()
 	 return new_pq;
 }
 
-priority_queue *add_sort (priority_queue *pq, node* new_node)
-{
-	node *current = pq->head;
-	while ((current->next != NULL) && (current->next->priority < new_node->priority))
-	{
-		current = current->next;
-	}
-	new_node->next = current->next;
-	current->next = new_node;
-	return pq;
-}
-
 void enqueue (priority_queue *pq, char i, int p)
 {
 	node *new_node = (node*) malloc (sizeof (node));
@@ -51,17 +39,53 @@ void enqueue (priority_queue *pq, char i, int p)
 	}
 	else
 	{
-		add_sort (pq, new_node);
+		node *current = pq->head;
+		while ((current->next != NULL) && (current->next->priority < new_node->priority))
+		{
+			current = current->next;
+		}
+		new_node->next = current->next;
+		current->next = new_node;
 	}
 }
+
+void print_queue (priority_queue *pq)
+{
+	node *aux = pq->head;
+	while (aux != NULL)
+	{
+		printf ("%c ", aux->item);
+		aux = aux->next;
+	}
+}
+
 void print_pre_order(node *pq)
-{	//printf("(");
+{	
 	if (pq != NULL) {
 		printf(" %c ", pq->item);
 		print_pre_order(pq->left);
 		print_pre_order(pq->right);
 	}
-	//printf(")");
+}
+
+priority_queue *add_sort (priority_queue *pq)
+{
+	node *current = pq->head->next;
+	node *previous = pq->head;
+	
+	while ((current != NULL) && (pq->head->priority > current->priority))
+	{
+		previous = current;
+		current = current->next;
+	}
+	if(pq->head != previous) {
+		node *aux = pq->head;
+		pq->head = pq->head->next;
+		previous->next = aux;
+		aux->next = current;	
+	}
+
+	return pq;
 }
 
 node* create_huffman_tree (priority_queue *pq)
@@ -73,47 +97,54 @@ node* create_huffman_tree (priority_queue *pq)
 		new_node->priority = pq->head->priority + pq->head->next->priority; 
 		new_node->left = pq->head;
 		new_node->right = pq->head->next;
-		//print_pre_order(pq->head);
-		//printf("\n\n");
-
 
 		if(pq->head->next->next != NULL) 
 		{
 			node *aux = pq->head->next->next;
 			pq->head->next->next = NULL;
 			pq->head->next = NULL;
-			//printf("ads\n");
-			pq->head = aux;
-			pq = add_sort (pq, new_node);
+			new_node->next = aux;
+			pq->head = new_node;
+
+			pq = add_sort (pq);
 		} else {
 			pq->head = new_node;
 		}
-		print_pre_order(pq->head);
-		printf("\n\n");
 	}
 }
 
 
 int main ()
 {
+	int caracter = 0;
+  	int array[256];
+	FILE *arquivo = fopen("japao.jpg", "r");
+	
+	if (arquivo == NULL) {
+		printf("File error\n");
+		return 0;
+	}
+
+	for(int j = 0; j < 256; j++) {
+		array[j] = 0;
+	}
+
+  	while(caracter != EOF) {
+  		caracter = fgetc(arquivo);
+  		array[caracter]++;
+
+  	}
+
 	int i, priority;
 	char item;
 	priority_queue *pq = create_priority_queue ();
-	for(i = 0; i < 6; i++) {
-		scanf("%c%d", &item, &priority);
-		getchar();
-		enqueue(pq, item, priority);
-
+	for(i = 0; i < 256; i++) {
+		if(array[i] > 0) {
+			enqueue(pq, i, array[i]);
+		}
 	} 
-	priority_queue *aux = pq;
-	/*while(aux->head != NULL) {
-		printf("%c %d\n", aux->head->item, aux->head->priority);
-		aux->head = aux->head->next;
-	}*/
-	// printf("ads\n");
 	create_huffman_tree(pq);
-	print_pre_order(pq->head);
-	printf("\n");
+	print_pre_order (pq->head);
 
 	return 0;
 }
