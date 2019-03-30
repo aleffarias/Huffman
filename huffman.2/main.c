7846cbf4f3d6306f5ca24e5b0e56878ebb6d295c
João Paulo Agostinho <43277315+Joao-paulo-ag@users.noreply.github.com>
2019-03-30 12:02:08 -0300

    compilação pronta (sem makefile)

---
+++ huffman.2/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "compress.h"
+#include "decompress.h"
+
+int main () {
+  int option;
+
+  puts ("HUFFMAN");
+  getchar ();
+
+  while (1) {
+    puts ("Digite:\n0 para compressao\n1 para descompressao\n2 para sair\n");
+
+    scanf ("%d", &option);
+    
+    while (option != 0 && option != 1 && option != 2) {
+      puts ("Digite corretamente!");
+      scanf ("%d", &option);
+    }
+
+    if (option == 2) {
+      break;
+    }
+
+    puts ("Nao vai durar nem uma hora!\n.\n.\n.");
+    
+    (option)? decompress() : compress ();
+    
+    getchar ();
+  }
+
+  return 0;
+}
